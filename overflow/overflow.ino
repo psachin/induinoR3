9175d42d6e8bd8fd4898e1472bd29d17eef59d71
psachin <iclcoolster@gmail.com>
2014-08-30 20:36:40 +0530

    Subject: Water overflow warning
    Description:
    
    - Basic prototype, Remote control reset is to be added.

---
+++ overflow/overflow.ino
@@ -0,0 +1,93 @@
+/* HC-SR04 Ultrasonic Sensor Interface*/
+
+int pingPin = 16; // A2 - Connected to both Trig & ECHO Pins of the Sensor
+int pinSpeaker = 10; //Set up a speaker on a PWM pin (digital 9, 10, or 11)
+
+int warningLED = 11; // choose the pin for the sound LED
+int warningLevel = 3; // RED LED
+
+void setup() {
+  pinMode(warningLED, OUTPUT);  // declare sndLED as output
+  pinMode(pinSpeaker, OUTPUT);
+  // initialize serial communication:
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  // establish variables for duration of the ping,
+  // and the distance result in inches and centimeters:
+  long duration, inches, cm;
+
+  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  pinMode(pingPin, OUTPUT);
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH); // Sending a High Pulse on the Trig Pin of the Sensor
+  delayMicroseconds(5);
+  digitalWrite(pingPin, LOW);
+
+  // The same pin is used to read the signal from the PING))): a HIGH
+  // pulse whose duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pinMode(pingPin, INPUT);
+  duration = pulseIn(pingPin, HIGH);
+
+  // convert the time into a distance
+  inches = microsecondsToInches(duration);
+  cm = microsecondsToCentimeters(duration);
+
+  Serial.print(inches);
+  Serial.print(" in ");
+  if(inches <= warningLevel) {
+      Serial.println("(WARNING!)");
+      startWarning();
+      // Serial.println();
+  }
+  // Serial.print(cm);
+  // Serial.print("cm");
+  Serial.println();
+  digitalWrite(warningLED, LOW);
+  delay(1000);
+}
+
+long microsecondsToInches(long microseconds)
+{
+  // According to Parallax's datasheet for the PING))), there are
+  // 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
+  // second).  This gives the distance travelled by the ping, outbound
+  // and return, so we divide by 2 to get the distance of the obstacle.
+  // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
+  return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds)
+{
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the
+  // object we take half of the distance travelled.
+  return microseconds / 29 / 2;
+}
+
+// duration in mSecs, frequency in hertz
+void playTone(long duration, int freq) {
+    duration *= 1000;
+    int period = (1.0 / freq) * 1000000;
+    long elapsed_time = 0;
+    while (elapsed_time < duration) {
+        digitalWrite(pinSpeaker,HIGH);
+        delayMicroseconds(period / 2);
+        digitalWrite(pinSpeaker, LOW);
+        delayMicroseconds(period / 2);
+        elapsed_time += (period);
+    }
+}
+
+void startWarning() {
+  digitalWrite(warningLED, HIGH);
+  playTone(300, 160);
+}
+
+
+
